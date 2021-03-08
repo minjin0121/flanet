fd7f37e3c48290610ed1b28b4529bd270a292981
최동식 <opert1564@naver.com>
2021-03-08 22:44:41 +0900

    feat: 🍎 Backend 주가 정보 분석 기능 추가
    Prophet 초안 모델
    
    ✅ Closes: #S04FIELD02-70

--- backend/app/database/crud.py
+++ backend/app/database/crud.py
@@ -27,3 +27,8 @@ def get_all_users(db: Session):
 # 스톡 목록 확인
 def get_all_stocks(db: Session):
     return db.query(models.Stock).all()
+
+
+# 주식 코드와 일치한 목록 확인
+def get_stocks_by_code(db: Session, code: str):
+    return db.query(models.Stock).filter(models.Stock.code == code).all()

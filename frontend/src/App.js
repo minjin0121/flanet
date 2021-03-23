2ee8b896adb5f5dc2df9cec673d5363bceeaa862
박예린 <linibell@naver.com>
2021-03-23 16:52:43 +0900

    feat: 🍎 Frontend Header, Footer 구현
    ✅ Closes: #S04FIELD02-155

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Header from "./components/index/Header";
+import Footer from "./components/index/Footer";
 import Home from "./pages/Home";
 import SignIn from "./pages/user/SignIn";
 import SignUp from "./pages/user/SignUp";
@@ -8,12 +10,14 @@ import BlockCoding from "./pages/blockcoding/BlockCoding";
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Switch>
         <Route exact path="/" component={Home} />
         <Route path="/signin" component={SignIn} />
         <Route path="/signup" component={SignUp} />
         <Route path="/blockcoding" component={BlockCoding} />
       </Switch>
+      <Footer />
     </BrowserRouter>
   );
 }

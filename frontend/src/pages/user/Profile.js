f7c6bee13b1f1fbe776fc4c725eccbaf0681a97a
fksk94 <fksk94@gmail.com>
2021-04-02 03:10:36 +0900

    feat: 🍎 마이페이지 초기 구현
    초기 구현 및 블록코딩 실행 저장 등 버튼 밑으로 내려오게 함
    
    ✅ Closes: #S04FIELD02-236

--- frontend/src/pages/user/Profile.js
+++ frontend/src/pages/user/Profile.js
@@ -1,9 +1,83 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getUserDataSet } from "../../actions/index";
+// import store from "../../index.js";
 
 function Profile() {
+  const dispatch = useDispatch();
+  const user = JSON.parse(
+    sessionStorage.getItem(
+      `firebase:authUser:${process.env.REACT_APP_FIREBASE_APIKEY}:[DEFAULT]`
+    )
+  );
+
+  dispatch(getUserDataSet(user.uid));
+  const userDataSets = useSelector((state) => state.userDataSets);
+
   return (
     <div className="profile">
-      디자인까지 하는 도산이를 위한 Profile 페이지~~~
+      <div className="storage">
+        <h1> Storage </h1>
+        <h2> 크롤링 데이터 </h2>
+        {Object.keys(userDataSets).length > 0 &&
+          userDataSets.user_data_set.map((item, idx) => (
+            <div key={idx}>
+              {(function () {
+                if (item.user_data_set_xml)
+                  return (
+                    <div className="historyBox">
+                      <span>{item.user_data_set_date}</span>
+                      {(function () {
+                        if (item.user_data_set_start)
+                          return (
+                            <span>
+                              {item.user_data_set_start} ~
+                              {item.user_data_set_end}
+                            </span>
+                          );
+                        else return <span>csv 데이터</span>;
+                      })()}
+                      <span>{item.user_data_set_name}</span>
+                      <span>{item.data_list_id}</span>
+                    </div>
+                  );
+                else return <span></span>;
+              })()}
+            </div>
+          ))}
+        <h2> 데이터 분석 및 예측 </h2>
+        <div className="historyBox">
+          <span>2021-04-02T03:20:01</span>
+          <span>csv 파일</span>
+          <span>1</span>
+        </div>
+        <h2> 저장된 모델 </h2>
+        <div className="historyBox">
+          <span>2021-04-02T03:20:01</span>
+          <span>ho 모델</span>
+          <span>1</span>
+        </div>
+      </div>
+      <div className="history">
+        <h1> History </h1>
+        {Object.keys(userDataSets).length > 0 &&
+          userDataSets.user_data_set.map((item, idx) => (
+            <div key={idx} className="historyBox">
+              <span>{item.user_data_set_date}</span>
+              {(function () {
+                if (item.user_data_set_start)
+                  return (
+                    <span>
+                      {item.user_data_set_start} ~ {item.user_data_set_end}
+                    </span>
+                  );
+                else return <span>csv 데이터</span>;
+              })()}
+              <span>{item.user_data_set_name}</span>
+              <span>{item.data_list_id}</span>
+            </div>
+          ))}
+      </div>
     </div>
   );
 }

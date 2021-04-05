8e9fdfd2212aacd368dbbd43932e4c9ce6f965c6
송민진 <minjin0121@gmail.com>
2021-04-05 20:25:03 +0900

    fix: 🐛 Frontend 블록 코딩 화면 Display 부분 SCSS 버그 수정
    - 코드 화면이 길어질 때마다 자식 div가 부모 div보다 커지는 현상 수정
    - 사용하지 않는 클래스 이름 정리
    
    ✅ Closes: #S04FIELD02-298

--- frontend/src/components/blockcoding/DisplayCode.js
+++ frontend/src/components/blockcoding/DisplayCode.js
@@ -3,15 +3,11 @@ import PropTypes from "prop-types";
 import { connect } from "react-redux";
 
 function DisplayCode({ code }) {
-  const myCode = code;
-
   return (
     <React.Fragment>
       <input name="nav" type="radio" className="nav" id="code" />
-      <div className="page code-page">
-        <div className="page-contents">
-          <div className="displayCode">{myCode}</div>
-        </div>
+      <div className="page">
+        <div className="page-contents displayCode">{code}</div>
       </div>
       <label className="nav" htmlFor="code">
         <span>코드</span>
